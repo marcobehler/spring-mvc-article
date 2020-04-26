ad1a8752e88ccd62ce56f3b36b60b124103bc3c2
marco <marco@marcobehler.com>
2020-04-26 10:11:13 +0200

    initial commit

---
+++ src/main/java/com/marcobehler/springmvcarticle/SpringMvcArticleApplication.java
@@ -0,0 +1,13 @@
+package com.marcobehler.springmvcarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcArticleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcobehler/springmvcarticle/SpringMvcArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcobehler.springmvcarticle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
