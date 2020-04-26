96c1c0b41ce7ab54f89c75ca1e64ee65ae7f5ce2
marco <marco@marcobehler.com>
2020-04-26 19:33:38 +0200

    FileUploadController

---
+++ src/main/java/com/marcobehler/springmvcarticle/FileDownloadController.java
@@ -0,0 +1,46 @@
+package com.marcobehler.springmvcarticle;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
+@Controller
+public class FileDownloadController {
+
+    @RequestMapping(value = "/download/{jpgName}", method = RequestMethod.GET)
+    public ResponseEntity<Resource> downloadJpg(
+            @PathVariable String jpgName) throws IOException {
+
+        //  Resource downloadResource = new InputStreamResource(soimeinputStream)
+        //  Resource downloadResource = new ByteArrayResource(someByteArray)
+        //  Resource downloadResource = new FileSystemResource(someFileSystem)
+        final ClassPathResource downloadResource = new ClassPathResource(jpgName);
+
+        if (!downloadResource.exists()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        HttpHeaders respHeaders = new HttpHeaders();
+
+        // 1. set the correct content type
+        respHeaders.setContentType(MediaType.IMAGE_JPEG);
+
+        // 2. set the correct content length, maybe stored in a db table
+        respHeaders.setContentLength(downloadResource.contentLength());
+
+        // 3. if you want to force downloads, otherwise attachments might be displayed directly in the brwoser
+        respHeaders.setContentDispositionFormData("attachment", jpgName);
+
+        return new ResponseEntity<>(downloadResource, respHeaders, HttpStatus.OK);
+    }
+}
