338708722caf6fccd5a3191acc8b3bc3526c7e60
marco <marco@marcobehler.com>
2020-04-28 13:20:30 +0200

    TomcatApplicationLauncher

---
+++ src/main/java/com/marcobehler/springmvcarticle/TomcatApplicationLauncher.java
@@ -0,0 +1,22 @@
+package com.marcobehler.springmvcarticle;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
+public class TomcatApplicationLauncher {
+
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat(); // <1>
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context ctx = tomcat.addContext("", null);
+        Wrapper servlet = Tomcat.addServlet(ctx, "myServlet", new MyServlet()); // <2>
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/*"); // <3>
+
+        tomcat.start(); // <4>
+    }
+}
