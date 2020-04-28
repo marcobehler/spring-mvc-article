8c2074ffa44e9ec31bdc2db191bdba18ad938d6e
marco <marco@marcobehler.com>
2020-04-28 13:29:17 +0200

    MyServletV2

--- src/main/java/com/marcobehler/springmvcarticle/MyServletV2.java
+++ src/main/java/com/marcobehler/springmvcarticle/MyServletV2.java
@@ -12,13 +12,13 @@ public class MyServletV2 extends HttpServlet {
         if (req.getRequestURI().equals("/")) {
             resp.setContentType("text/html");
             resp.getWriter().print("<html><head></head><body><h1>Welcome!</h1><p>This is a very cool page!</p></body></html>");
-        } else if (req.getRequestURI().startsWith("/api/users/")) {
+        } else if (req.getRequestURI().startsWith("/api/users/")) { // <1>
 
-            Integer prettyFragileUserId = Integer.valueOf(req.getRequestURI().lastIndexOf("/") + 1);
+            Integer prettyFragileUserId = Integer.valueOf(req.getRequestURI().lastIndexOf("/") + 1); // <2>
 
             // User user = dao.findUser(prettyFragileUserId)
 
-            resp.setContentType("application/json");
+            resp.setContentType("application/json"); // <3>
             resp.getWriter().print("{\n" +
                     "  \"id\":" + prettyFragileUserId + ",\n" +
                     "  \"age\": 55,\n" +
