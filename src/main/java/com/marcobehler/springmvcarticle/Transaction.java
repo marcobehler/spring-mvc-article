d367a8142f362e3d7efc1290d95bae076c8eaeff
marco <marco@marcobehler.com>
2020-04-28 15:18:47 +0200

    BankController

--- src/main/java/com/marcobehler/springmvcarticle/Transaction.java
+++ src/main/java/com/marcobehler/springmvcarticle/Transaction.java
@@ -1,15 +1,35 @@
 package com.marcobehler.springmvcarticle;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 public class Transaction {
 
-    public LocalDateTime occurred;
+
+    private LocalDateTime occurred;
 
     private String description;
 
     private Integer id;
 
+    private Integer amount;
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy hh:mm")
     public LocalDateTime getOccurred() {
         return occurred;
     }
@@ -32,5 +52,19 @@ public class Transaction {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Transaction(LocalDateTime occurred, String description, Integer id, Integer amount) {
+        this.occurred = occurred;
+        this.description = description;
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public static void main(String[] args) throws JsonProcessingException {
+
+        List<Transaction> tx = Arrays.asList(new Transaction(LocalDateTime.now(), "McDonalds", 1, 10), new Transaction(LocalDateTime.now(), "Burger King", 2, 15));
+
+        System.out.println(new ObjectMapper()   .registerModule(new JSR310Module()).writeValueAsString(tx));
     }
 }
